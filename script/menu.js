32490911a17aa56bf5ccd1db4f83777fc8242a3f
Mateo Vargas <142639109+dokiun@users.noreply.github.com>
2024-05-13 16:31:53 -0500

    Add files via upload

---
+++ script/menu.js
@@ -0,0 +1,9 @@
+document.getElementById("dropdownButton").addEventListener("click", function(event) {
+  event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+  var dropdownMenu = document.getElementById("dropdownMenu");
+  if (dropdownMenu.style.display === "none") {
+    dropdownMenu.style.display = "inline-block";
+  } else {
+    dropdownMenu.style.display = "none";
+  }
+});
